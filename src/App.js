3e20f51367cfa3b97cd289f2c4963592cb03c6b8
thulaninhla <thulaninhlapo2@gmail.com>
2021-08-12 22:57:47 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import { Router } from "@reach/router";
 import SignIn from "./Components/SignIn";
 import SignUp from "./Components/SignUp";
+import { Container } from "react-boostrap"
 import Application from "./Components/Application";
 import UserProvider from "./providers/UserProvider";
 import ProfilePage from "./Components/ProfilePage";
