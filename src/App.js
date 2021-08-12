e7ae17ccaffe4e9ed90e96fd903568d846e38de6
thulaninhla <thulaninhlapo2@gmail.com>
2021-08-12 21:06:47 +0200

    2 files modified, 7 added, 6 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
-import "./style.css";
-
-
-export default function App() {
+  
+import React, { useContext } from "react";
+import { Router } from "@reach/router";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
+import Application from "./Components/Application";
+import UserProvider from "./providers/UserProvider";
+import ProfilePage from "./Components/ProfilePage";
+import { UserContext } from "./providers/UserProvider";
+function App() {
+  
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <UserProvider>
+      <Application />
+    </UserProvider>
   );
 }
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,60 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { functions } from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBr6Y6dGzsV2qg71ldTmxZ_nS26XBBjRs4",
+  authDomain: "project-pro-41396.firebaseapp.com",
+  databaseURL: "https://project-pro-41396-default-rtdb.firebaseio.com",
+  projectId: "project-pro-41396",
+  storageBucket: "project-pro-41396.appspot.com",
+  messagingSenderId: "51857395050",
+  appId: "1:51857395050:web:3e1c36293fd7ae3d67e913"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(provider);
+};
+
+export const generateUserDocument = async (user, additionalData) => {
+  if (!user) return;
+
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { email, displayName, photoURL } = user;
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error("Error creating user document", error);
+    }
+  }
+  return getUserDocument(user.uid);
+};
+
+const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    const userDocument = await firestore.doc(`users/${uid}`).get();
+
+    return {
+      uid,
+      ...userDocument.data()
+    };
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
+};
